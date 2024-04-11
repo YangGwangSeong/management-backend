0acf8d78d72f5cef5cec8728c7d7b8cd4dc93d16
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-11 03:22:15 -0700

    (#4) :sparkles: feat: prettier format 적용확인
    - 기능에 대한 자세한 설명 ......

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,10 +6,10 @@ import { AppService } from './app.service';
 export class AppController {
 	constructor(private readonly appService: AppService) {}
 
-	@Get()
-	getHello(): string {
-		return this.appService.getHello();
-	}
+@Get()
+getHello(): string {
+	return this.appService.getHello();
+}
 
 	@Get()
 	helloword() {}
