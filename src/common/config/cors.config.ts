0a5771d9902f7a7ac2cb9807b04e5c3c6ac620b1
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-23 01:37:56 -0700

    (#8) :sparkles: feat: cors 설정
    - config 폴더 생성후 cors.config.ts 파일에서 관리

---
+++ src/common/config/cors.config.ts
@@ -0,0 +1,7 @@
+export const CorsConfig = {
+	origin: true,
+	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	preflightContinue: false,
+	credentials: true,
+	allowedHeaders: 'Content-Type, Accept, Authorization',
+};
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 
+import { CorsConfig } from './common/config/cors.config';
 import { ENV_APPLICATION_PORT } from './constants/env-keys.const';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	// cors
+	app.enableCors(CorsConfig);
+
 	await app.listen(ENV_APPLICATION_PORT);
 }
 bootstrap();
