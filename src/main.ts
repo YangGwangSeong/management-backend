076afaf58add2b8c4f17665ec1f3435be8264f6e
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-23 01:33:00 -0700

    (#8) :sparkles: feat: 애플리케이션 포트 환경변수 추가
    - constnas 폴더를 이용한 환경변수 관리

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 
+import { ENV_APPLICATION_PORT } from './constants/env-keys.const';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+	await app.listen(ENV_APPLICATION_PORT);
 }
 bootstrap();
