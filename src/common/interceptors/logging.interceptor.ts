1bd46d08c036d13a2cdc9e3b77cd086fc8a6a1f3
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-23 03:25:21 -0700

    (#8) :sparkles: feat: route-level에 적용하기 위한 TimeoutInterceptor와 LoggingInterceptor 추가
    - route-level에 적용하기 위한 TimeoutInterceptor와 LoggingInterceptor 추가

---
+++ src/common/interceptors/logging.interceptor.ts
@@ -0,0 +1,35 @@
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import {
+	Injectable,
+	NestInterceptor,
+	ExecutionContext,
+	CallHandler,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+	constructor(private readonly configService: ConfigService) {}
+	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+		const request = context.switchToHttp().getRequest();
+		const { path } = request;
+
+		return next.handle().pipe(
+			tap(() => {
+				if (
+					this.configService.get<string>('NODE_ENV')?.toLowerCase() !== 'test'
+				) {
+					const requestToResponse: `${number}ms` = `${
+						Date.now() - request.now
+					}ms`;
+					console.log(
+						`logging\n${request.method} ${path} ${requestToResponse}\n` +
+							`currentTime : ${new Date()}]\n`,
+					);
+				}
+			}),
+		);
+	}
+}
