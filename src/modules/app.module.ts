eb0a2c267ca5bb0cc522bb3b9dad059092f77887
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-23 02:32:35 -0700

    (#8) :sparkles: feat: nestjs/config 설정하기
    - nestjs/config 설정하기

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,15 @@
 import { AppController } from '@/controllers/app.controller';
 import { AppService } from '@/providers/app.service';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: [`${__dirname}/../../.${process.env.NODE_ENV}.env`],
+			isGlobal: true,
+		}),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
