11aa2fa4893ed8c15d6e580b122cae0eaf8c6a97
YangGwangSeong <rhkdtjd_12@naver.com>
2024-04-25 01:40:05 -0700

    (#8) :sparkles: feat: swagger 세팅하기
    - swagger 세팅하기

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import helmet from 'helmet';
 
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { CorsConfig } from './common/config/cors.config';
 import {
@@ -24,6 +25,13 @@ dotenv.config({
 				: '.development.env',
 	),
 });
+
+const getSwaggerOptions = () => ({
+	swaggerOptions: {
+		persistAuthorization: true, //웹 페이지를 새로고침을 해도 Token 값 유지
+	},
+});
+
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	app.set('trust proxy', true);
@@ -39,6 +47,26 @@ async function bootstrap() {
 
 	// set global prefix
 	app.setGlobalPrefix(String(process.env[ENV_GLOBAL_PREFIX]));
+
+	// swagger
+	const config = new DocumentBuilder()
+		.setTitle('fam API')
+		.setDescription('연차관리 프로젝트를 위한 API 문서')
+		.setVersion('1.0.0')
+		.addBearerAuth(
+			{
+				type: 'http',
+				scheme: 'bearer',
+				name: 'JWT',
+				in: 'header',
+			},
+			'accessToken',
+		)
+		.addSecurityRequirements('accessToken')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api/v1/swagger', app, document, getSwaggerOptions());
 
 	await app.listen(Number(process.env[ENV_APPLICATION_PORT]));
 }
